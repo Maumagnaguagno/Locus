53687dc2686622b304e3fc059c3862094dacf55d
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-03-09 03:11:33 -0300

    Tests are expected to run now
    Changed path style in tests to work on Windows and Linux. Added JRuby
    and MRI 2.1 to play with.

--- test_locus.rb
+++ test_locus.rb
@@ -4,10 +4,9 @@ require './Locus.rb'
 class Test_Locus < Test::Unit::TestCase
 
   def compare_files(filename)
-    print Dir.pwd
     # Result is compared with original file tested with Jason
-    result = Locus.to_java(".\\examples\\#{filename}.esl")
-    original = IO.read(".\\examples\\#{filename}.java")
+    result = Locus.to_java("examples/#{filename}.esl")
+    original = IO.read("examples/#{filename}.java")
     # Ignore timestamp
     result.sub!(/Generated at .*$/,'')
     original.sub!(/Generated at .*$/,'')
@@ -15,14 +14,14 @@ class Test_Locus < Test::Unit::TestCase
   end
 
   def test_room
-    compare_files('Room\RoomEnv')
+    compare_files('Room/RoomEnv')
   end
 
   def test_bakery_react
-    compare_files('BakeryReact\Bakery')
+    compare_files('BakeryReact/Bakery')
   end
 
   def test_bakery_loop
-    compare_files('BakeryLoop\Bakery')
+    compare_files('BakeryLoop/Bakery')
   end
 end
